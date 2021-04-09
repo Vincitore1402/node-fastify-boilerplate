d7d85dd0fb6273ce8144485a0c840ebfe55eb3f0
Vincitore1402 <vincitoremonster1402@gmail.com>
2021-04-09 16:33:00 +0300

    universities service implemented & model updated

--- src/db/models/mongoose/University.js
+++ src/db/models/mongoose/University.js
@@ -5,6 +5,14 @@ const University = new mongoose.Schema({
   city: String,
   country: String,
   subjects: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('University', University);
--- src/services/universities.service.js
+++ src/services/universities.service.js
@@ -1,6 +1,89 @@
+const {
+  omit, map, pipe, get,
+  omitBy, merge, isNil,
+} = require('lodash/fp');
+
+const sanitizeResponse = omit(['__v']);
+
 class UniversitiesService {
-  constructor(...args) {
-    this.params = args;
+  constructor(options = {}) {
+    this.Model = options.model || {};
+  }
+
+  async list() {
+    const data = await this.Model
+      .find()
+      .lean();
+
+    return map(sanitizeResponse, data);
+  }
+
+  /**
+   * Returns a single university item if it exists, otherwise fails.
+   *
+   * @param id
+   */
+  async get(id) {
+    const data = await this.Model
+      .findOne({ _id: id })
+      .lean();
+
+    if (!data) throw new Error(`University with id: ${id} not found`);
+
+    return sanitizeResponse(data);
+  }
+
+  /**
+   * Creates new university using provided payload
+   *
+   * @param payload - university item which should be created
+   */
+  async create(payload) {
+    const response = await this.Model.create({
+      ...payload,
+    });
+
+    return pipe(
+      get('_doc'),
+      sanitizeResponse,
+    )(response);
+  }
+
+  /**
+   * Updates university by id
+   *
+   * @param id
+   * @param payload
+   */
+  async update(id, payload = {}) {
+    const item = this.get(id);
+
+    const updatePayload = pipe(
+      omitBy(isNil),
+      (it) => ({ ...it, updatedAt: new Date() }),
+      merge(item),
+    )(payload);
+
+    const params = { new: true, lean: true };
+
+    const response = await this.Model.findByIdAndUpdate(
+      id,
+      updatePayload,
+      params,
+    );
+
+    return sanitizeResponse(response);
+  }
+
+  /**
+   * Removes university by id
+   *
+   * @param id
+   */
+  async remove(id) {
+    await this.Model.findByIdAndRemove(id);
+
+    return { id };
   }
 }
 
